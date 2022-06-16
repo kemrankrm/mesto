0c18495483ef9d673aa91a1414c1d6120737b4f3
Kemran Karimov <kemran.karimov@gmail.com>
2022-06-16 16:14:22 +0600

    Last issues fixed

--- scripts/script.js
+++ scripts/script.js
@@ -3,30 +3,28 @@ let editButton = document.querySelector('.profile__edit-button');
 let formElement = document.querySelector('.popup');
 let formElementClose = document.querySelector('.popup__close-button');
 let formSubmitButton = document.querySelector('.popup__submit-button');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_job');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__description');
-
-// Input Values setup
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
 
 // Event Listeners
 editButton.addEventListener('click', formOpen);
 formElementClose.addEventListener('click', formClose);
 formSubmitButton.addEventListener('click', formSubmitHandler);
 
-// Functions
+// Functions (я до конца не понимаю что именно от меня требуется).
 function formOpen(){
     formElement.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
 }
 
 function formClose(){
     formElement.classList.remove('popup_opened');
 }
 
-function formSubmitHandler(evt) {
+function formSubmitHandler(evt){
     evt.preventDefault();
     profileName.textContent = `${nameInput.value}`;
     profileJob.textContent = `${jobInput.value}`;
