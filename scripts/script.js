20e1e611ca9a0afd4410699f21291460cee5c44e
Kemran Karimov <kemran.karimov@gmail.com>
2022-06-26 18:35:53 +0600

    landing ready

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,43 @@
 // Variables
 let editButton = document.querySelector('.profile__edit-button');
+let addButton = document.querySelector('.profile__add-button');
 let formElement = document.querySelector('.popup');
+let addFormElement = document.querySelector('.popup-new-place');
 let formElementClose = document.querySelector('.popup__close-button');
 let formSubmitButton = document.querySelector('.popup__submit-button');
+let newPlaceSubmitButton = document.querySelector('#np-submit');
+let newPlaceCloseButton = document.querySelector('#np-close');
 let nameInput = document.querySelector('.popup__input_type_name');
 let jobInput = document.querySelector('.popup__input_type_job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__description');
 
+
 // Event Listeners
 editButton.addEventListener('click', formOpen);
+addButton.addEventListener('click', addFormOpen);
 formElementClose.addEventListener('click', formClose);
 formSubmitButton.addEventListener('click', formSubmitHandler);
+newPlaceCloseButton.addEventListener('click', newPlaceFormClose);
+newPlaceSubmitButton.addEventListener('click', newPlaceSubmit);
 
-// Functions (я до конца не понимаю что именно от меня требуется).
+// Functions.
 function formOpen(){
     formElement.classList.add('popup_opened');
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }
 
+function addFormOpen(){
+    addFormElement.classList.add('popup-new-place_open');
+}
+
 function formClose(){
     formElement.classList.remove('popup_opened');
+}
+
+function newPlaceFormClose(){
+    addFormElement.classList.remove('popup-new-place_open');
 }
 
 function formSubmitHandler(evt){
@@ -30,3 +46,9 @@ function formSubmitHandler(evt){
     profileJob.textContent = `${jobInput.value}`;
     formElement.classList.remove('popup_opened');
 }
+
+function newPlaceSubmit(evt){
+    evt.preventDefault();
+    addFormElement.classList.remove('popup-new-place_open');
+}
+
